18d4bf7ac16cbe316ae2d24121e797696d6e3f4a
Luiza Hackenhaar Naziazeno <142232479+luizahackenhaarnaziazeno@users.noreply.github.com>
2026-04-02 15:33:21 -0300

    Refactor Paciente class and update comments

--- TrabalhoFinal-ClinicaMedica-Gabrielle Guarani e Luiza Hackenhaar/Paciente.java
+++ TrabalhoFinal-ClinicaMedica-Gabrielle Guarani e Luiza Hackenhaar/Paciente.java
@@ -1,6 +1,6 @@
-//classe paciente
+
 public class Paciente { 
-    //informações necessárias para os dados da classe Paciente
+  
     private String nome;
     private String cpf;
     private String telefone;
@@ -16,7 +16,7 @@ public class Paciente {
         this.diagnostico = diagnostico;
         this.numConsultasMes = numConsultasMes;
     }
-    //Métodos para enviar as informações do paciente
+    
     public void setTelefone(String telefone) { this.telefone = telefone; }
 
     public void setConvenio(String convenio) {this.convenio = convenio; }
@@ -28,13 +28,13 @@ public class Paciente {
     public void setNumConsultasMes(int numConsultasMes) { this.numConsultasMes = numConsultasMes; }
 
     public int getNumConsultasMes() { return numConsultasMes;}
-    //Método que calcula o valor total de consultas desse paciente
+    
     public int calcularValorTotalConsulta(int valorConsulta) { return numConsultasMes * valorConsulta; }
-    //Método que printa todos os métodos dessa classe na tela
+    
     public String toString() {
         return "Nome: " + nome + "\nCPF: " + cpf + "\nTelefone: " + telefone + "\nConvênio: " + convenio + "\nDiagnóstico: " + getDiagnosticoTipos() + "\nNúmero de consultas do mês: " + numConsultasMes;
     }
-    //declaração do método para pegar informações do diagnostico do paciente
+   
     private String getDiagnosticoTipos() { 
         switch (diagnostico) {
             case 1:
